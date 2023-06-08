c08abeef53ef98d42ce2406fcba1b259efc988b5
Champ2001 <sarnavamohanta2001@gmail.com>
2023-06-09 03:35:47 +0530

    Codes

---
+++ src/utils/fetchFromAPI.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+
+export const BASE_URL = 'https://youtube-v31.p.rapidapi.com';
+
+const options = {
+  params: {
+    maxResults: 50,
+  },
+  headers: {
+    'X-RapidAPI-Key': '9fe8f0d57amsha7dcbd969dbd1e0p129c39jsn8625373574d1',
+    'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com',
+  },
+};
+
+export const fetchFromAPI = async (url) => {
+  const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+
+  return data;
+};
